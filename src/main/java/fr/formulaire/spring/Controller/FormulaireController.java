2a0ef2645ce5c36d41c72939866b8f757f44bd04
Admin Stagiaire <Admin Stagiaire@P7720-56D>
2023-07-30 22:05:17 +0200

    Ajout des controls de validation des formulaires

--- src/main/java/fr/formulaire/spring/Controller/FormulaireController.java
+++ src/main/java/fr/formulaire/spring/Controller/FormulaireController.java
@@ -2,11 +2,12 @@ package fr.formulaire.spring.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.formulaire.spring.DTO.FormulaireDTO;
+import jakarta.validation.Valid;
 
 @Controller
 public class FormulaireController {
@@ -20,7 +21,11 @@ public class FormulaireController {
     }
     
     @PostMapping("/formulaire")
-    public String soumttreformulaire( @ModelAttribute FormulaireDTO formulairedto, Model model ) {
+    public String soumttreformulaire( @Valid FormulaireDTO formulairedto, BindingResult result, Model model ) {
+        
+        if(result.hasErrors()) {
+            return"formulaire";
+        }
         model.addAttribute("formulairedto", formulairedto);
         return"resformulaire";
     }
--- src/main/java/fr/formulaire/spring/DTO/FormulaireDTO.java
+++ src/main/java/fr/formulaire/spring/DTO/FormulaireDTO.java
@@ -1,8 +1,25 @@
 package fr.formulaire.spring.DTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class FormulaireDTO {
     
+    @NotNull
+    @Size(min=2, max=50)
     private String nom;
+    
+    @NotNull
+    @Size(min=2, max=50)
+    private String prenom;
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 
     public String getNom() {
         return nom;
